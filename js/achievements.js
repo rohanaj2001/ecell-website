7c8767e58be865c5729e89f8cc3df1cd882836f9
rohanaj2001 <rohanaj2001@gmail.com>
2022-05-01 22:55:28 +0530

    before updating navbar

--- js/achievements.js
+++ js/achievements.js
@@ -2,9 +2,11 @@ var startups = document.getElementById('startups')
 var competitions = document.getElementById('competitions')
 var events = document.getElementById('events')
 var contentBox = document.getElementById('contentBox')
-
-function startupsFn(){
-    contentBox.innerHTML=` <div class="achievementsContentBox p5 d-flex align-items-center justify-content-center text-white">
+var contentHeading = document.getElementById('contentHeading')
+function startupsFn() {
+    contentHeading.innerText="STARTUPS"
+    contentBox.innerHTML = `<div class="greyBorder"><div></div></div> 
+    <div class="achievementsContentBox p5 d-flex align-items-center justify-content-center ">
     <div class="left d-flex align-items-center justify-content-center flex-column">
         <div class="card text-black" style="width: 18rem;">
             <div class="card-body">
@@ -36,7 +38,8 @@ function startupsFn(){
     </div>
 
 </div>
-<div class="achievementsContentBox p5 d-flex align-items-center justify-content-center text-white">
+<div class="greyBorder"><div></div></div>
+<div class="achievementsContentBox p5 d-flex align-items-center justify-content-center">
     <div class="left d-flex align-items-center justify-content-center flex-column">
         <div class="card text-black" style="width: 18rem;">
             <div class="card-body">
@@ -66,8 +69,9 @@ function startupsFn(){
             </div>
         </div>
     </div>
-
-</div><div class="achievementsContentBox p5 d-flex align-items-center justify-content-center text-white">
+</div>
+<div class="greyBorder"><div></div></div>
+<div class="achievementsContentBox p5 d-flex align-items-center justify-content-center ">
 <div class="left d-flex align-items-center justify-content-center flex-column">
     <div class="card text-black" style="width: 18rem;">
         <div class="card-body">
@@ -97,16 +101,18 @@ function startupsFn(){
         </div>
     </div>
 </div>
-
 </div>
- `
+<div class="greyBorder"><div></div></div>
+`
 }
 
-window.addEventListener('load',startupsFn)
+window.addEventListener('load', startupsFn)
 startups.addEventListener('click', startupsFn)
 
-competitions.addEventListener('click', ()=>{
-    contentBox.innerHTML=`</div><div class="achievementsContentBox p5 d-flex align-items-center justify-content-center text-white">
+competitions.addEventListener('click', () => {
+    contentHeading.innerText="COMPETITIONS"
+    contentBox.innerHTML = `<div class="greyBorder"><div></div></div> 
+<div class="achievementsContentBox p5 d-flex align-items-center justify-content-center">
     <div class="left d-flex align-items-center justify-content-center flex-column">
         <div class="card text-black" style="width: 18rem;">
             <div class="card-body">
@@ -134,11 +140,48 @@ competitions.addEventListener('click', ()=>{
                 <p class="card-text">Some quick example text to build on the card title and make up
                     the bulk of the card's content.</p>
             </div>
-        </div>`
+        </div>
+    </div>
+</div>
+<div class="greyBorder"><div></div></div>
+<div class="achievementsContentBox p5 d-flex align-items-center justify-content-center ">
+<div class="left d-flex align-items-center justify-content-center flex-column">
+    <div class="card text-black" style="width: 18rem;">
+        <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-black">2008</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up
+                the bulk of the card's content.</p>
+        </div>
+    </div>
+</div>
+<div class="right d-flex align-items-center justify-content-center flex-column">
+    <div class="card text-black" style="width: 18rem;">
+        <div class="card-body">
+    <h6 class="card-subtitle mb-2 text-black"><span>
+    <ion-icon name="briefcase"></ion-icon>
+    </span>2008</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up
+                the bulk of the card's content.</p>
+        </div>
+    </div>
+    <div class="card text-black" style="width: 18rem;">
+        <div class="card-body">
+        <h6 class="card-subtitle mb-2 text-black"><span>
+        <ion-icon name="briefcase"></ion-icon>
+        </span>2008</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up
+                the bulk of the card's content.</p>
+        </div>
+    </div>
+</div>
+</div>
+<div class="greyBorder"><div></div></div>`
 })
 
-events.addEventListener('click', ()=>{
-    contentBox.innerHTML=`</div><div class="achievementsContentBox p5 d-flex align-items-center justify-content-center text-white">
+events.addEventListener('click', () => {
+    contentHeading.innerText="EVENTS"
+    contentBox.innerHTML = `<div class="greyBorder"><div></div></div> 
+    <div class="achievementsContentBox p5 d-flex align-items-center justify-content-center ">
     <div class="left d-flex align-items-center justify-content-center flex-column">
         <div class="card text-black" style="width: 18rem;">
             <div class="card-body">
@@ -166,5 +209,9 @@ events.addEventListener('click', ()=>{
                 <p class="card-text">Some quick example text to build on the card title and make up
                     the bulk of the card's content.</p>
             </div>
-        </div>`
+        </div>
+    </div>
+    </div>
+    <div class="greyBorder"><div></div></div>`
 })
+var anchors = document.getElementsByClassName("hexLinks")
